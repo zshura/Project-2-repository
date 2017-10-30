f8587fb8de58a5594fdd6a4f247a4682fd938f0e
DESKTOP-UJIMONJ\zofia <zhs17@hampshire.edu>
2017-10-30 15:03:24 -0400

    Part 1

---
+++ Project 2.1/Assets/CubeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+	public int x, y; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnMouseDown () {
+		GameController.ProcessClick (gameObject);
+	}
+}
---
+++ Project 2.1/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject cubePrefab;
+	Vector3 cubePosition;  
+	public static GameObject selectedCube;
+	GameObject myCube;
+
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 16; i++) {
+			cubePosition = new Vector3 (i * 2 - 16, 0, 0);
+			Instantiate (cubePrefab, cubePosition, Quaternion.identity);
+		}
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	}
+		public static void ProcessClick (GameObject clickedCube) {
+			if (selectedCube != null) {
+				selectedCube.GetComponent<Renderer>().material.color = Color.white;
+			}
+
+			clickedCube.GetComponent<Renderer> ().material.color = Color.red;
+			selectedCube = clickedCube; 
+		}
+
+}
